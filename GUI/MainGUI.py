44f5a5be10bbc8493eea497a534115d16c4171a9
Guldhammer <Guldhammer@h392.aau1x-3.klient.audt.site.aau.dk>
2018-12-04 13:35:55 +0100

    added main GUI and __init__ file

---
+++ GUI/MainGUI.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+
+LARGE_FONT = ("Times New Roman", 12)
+
+
+class Horetester(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self) #indeholder alt vi indsætter
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0,column = 0, sticky="nsew")
+
+
+        self.show_frame(StartPage)
+
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__ (self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start side", font = LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        Button_1 = tk.Button(self, text ="Kom igang",
+        command = lambda: controller.show_frame(PageOne))
+        Button_1.pack()
+
+class PageOne(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="vaelg køn:", font = LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        Button_2 = tk.Button(self, text ="Tilbage",
+        command = lambda: controller.show_frame(StartPage))
+        Button_2.pack()
+
+app = Horetester()
+app.mainloop()
