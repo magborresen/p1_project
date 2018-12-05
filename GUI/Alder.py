1f9b03f49c4db410e1f49ab572d4f651f9f8c1a9
Magnus Borresen <magnusborresen@h198.natout.aau.dk>
2018-12-05 12:09:59 +0100

    added functionality to alder.py & changed variables.json

--- GUI/Alder.py
+++ GUI/Alder.py
@@ -4,33 +4,32 @@ import InfoScreen
 import json
 TITLE_FONT = ("Verdana", 20, "bold")
 
-with open('../variables.json', 'r') as f:
-    data = json.load(f)
 
 class Age(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         label = tk.Label(self, text="Hvor gammel er du?", font="TITLE_FONT")
-        label.pack(pady=10,padx=10)
+        label.pack(pady=10, padx=10)
 
-        label_1 = tk.Label(self, text="Indtast alder:", font= "TITLE_FONT")
-        age_entry = tk.Entry(self)
+        label_1 = tk.Label(self, text="Indtast alder:", font="TITLE_FONT")
+        self.age_entry = tk.Entry(self)
 
         label_1.pack()
-        age_entry.pack()
+        self.age_entry.pack()
 
-        age_answer = age_entry.get()
-
-
+        if self.age_entry.get() == "":
+            print (self.age_entry.get())
 
         button1 = ttk.Button(self, text="Næste",
-                            command=lambda: [master.switch_frame(InfoScreen.Information),
-                                            self.update_json(age_answer)])
+                            command=lambda: [self.update_json(),
+                                             master.switch_frame(InfoScreen.Information)])
         button1.pack()
 
-    def update_json(self, age):
-
-        with open('../variables.json', 'w') as f:
+    def update_json(self):
+        age_answer = self.age_entry.get()
+        with open('../variables.json', 'r+') as f:
             data = json.load(f)
-            data["Age"] = int(age)
+            data["Age"] = int(age_answer)
+            f.seek(0)
             json.dump(data, f)
+            f.truncate()
