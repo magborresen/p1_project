5be6c86d0b45196ab62ffe39529f63414d651b5c
Magnus Borresen <magnusborresen@MagBook-Pro-7.local>
2018-12-10 22:02:13 +0100

    Test now works

--- GUI/Test.py
+++ GUI/Test.py
@@ -6,6 +6,9 @@ import pygame
 import time
 import json
 import math
+import sys
+sys.path.append("../Result/")
+import calculate_result
 TITLE_FONT = ("Verdana", 20, "bold")
 
 
@@ -26,13 +29,15 @@ class Test(tk.Frame):
         self.test_num = 0
         self.frequency = 250
         self.ear = 0
+        self.decibel = 0
 
     def hort(self):
         self.test_num += 1
         print (self.frequency)
-        if self.test_num > 1:
+        if self.test_num > 1 and self.decibel <= 8000:
             volume = pygame.mixer.music.get_volume()
-            self.update_json(self.ear, volume)
+            self.convert_to_db(volume)
+            self.update_json(self.ear, self.decibel)
             print (volume)
 
         if self.test_num == 1:
@@ -73,6 +78,8 @@ class Test(tk.Frame):
             self.play_left_ear()
         elif self.test_num == 13:
             pygame.mixer.music.stop()
+            self.after_cancel(self.increase_volume)
+            calculate_result.calc_mean()
             self.master.switch_frame(Resultat.Result)
 
     # Funktion, som faar lydstyrken til at stige
@@ -82,9 +89,9 @@ class Test(tk.Frame):
         if volume < 1.0:
             new_volume = volume + 0.01
             pygame.mixer.music.set_volume(new_volume)
-            self.after(1000, self.increase_volume)
+            self.after(2000, self.increase_volume)
 
-    def play_right_ ear(self):
+    def play_right_ear(self):
         volume = 0.000
         pygame.mixer.music.load("../Frekvensafspiller/justerede_lydfiler/" + str(self.frequency) + "Hz_R.mp3")
         pygame.mixer.music.play(10)
@@ -101,23 +108,22 @@ class Test(tk.Frame):
         self.frequency = self.frequency * 2
         self.increase_volume()
 
-        # Funktion som omregner volumen til decibel ud fra funktion fra tendenslinje
-    def omregner(self):
-        decibel = 0
-        decibel = 8.87864 * math.log(volume) + 79.31112
+    # Funktion som omregner volumen til decibel ud fra funktion fra tendenslinje
+    def convert_to_db(self, volume):
+        self.decibel = 8.87864 * math.log(volume) + 79.31112
 
-    def update_json(self, ear, volume):
+    def update_json(self, ear, decibel):
         if ear == 0:
             with open('../variables.json', 'r+') as f:
                 data = json.load(f)
-                data["right_ear_test"][str(self.frequency)] = volume
+                data["right_ear_test"][str(self.frequency)] = decibel
                 f.seek(0)
                 json.dump(data, f)
                 f.truncate()
         elif ear == 1:
             with open('../variables.json', 'r+') as f:
                 data = json.load(f)
-                data["left_ear_test"][str(self.frequency)] = volume
+                data["left_ear_test"][str(self.frequency)] = decibel
                 f.seek(0)
                 json.dump(data, f)
                 f.truncate()
