8cee0a2820c9930ea5f84bf535e87af64ce98651
KristofferTheMachine <kkorsg18@student.aau.dk>
2018-12-11 12:53:59 +0100

    Pychache shit

--- GUI/Test.py
+++ GUI/Test.py
@@ -1,31 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
 import Resultat
-<<<<<<< HEAD
-=======
-import Alder
 import pygame
-import time
->>>>>>> 7348a06294273d98383a526c04f968bce7b4cdeb
 
 TITLE_FONT = ("Verdana", 20, "bold")
 
 class Test(tk.Frame):
     def __init__(self,master):
         tk.Frame.__init__(self, master)
-        label = tk.Label(self, text="Høretest i gang...", font = TITLE_FONT)
-        label.place(x=400, y=150)
+        label = tk.Label(self, width=20, height= 5,text="Høretest i gang...", font = TITLE_FONT)
+        label.place(x=200, y=80)
         LydHort = tk.Button(self,width = 20, height = 5, text ="Hørt",
         command = lambda: master.switch_frame(Resultat.Result))
 
-        Button_1.pack()
+        LydHort.place(x=400,y=240)
 
         # Initier pygame, som er dimsen, som afspiller lydfilerne
         pygame.mixer.init()
 
     #Setup til knappen
-
-    def knaptrykket(self):
 
 
     # Funktion, som faar lydstyrken til at stige
@@ -43,10 +36,10 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/250Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
-            for ten_loops in range(1000):
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/250Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
+            for ten_loops in range (1000):
                 if knaptrykket():
                     sluttid = time.time()
                     resultattid = sluttid - starttid
@@ -61,9 +54,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/250Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/250Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -79,9 +72,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/500Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/500Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -97,9 +90,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/500Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/500Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -115,9 +108,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/1000Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/1000Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -133,9 +126,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/1000Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/1000Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -151,9 +144,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/2000Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/2000Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -169,9 +162,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/2000Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/2000Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -187,9 +180,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/4000Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/4000Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -205,9 +198,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/4000Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/4000Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -223,9 +216,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/8000Hz_1_R.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/8000Hz_1_R.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -241,9 +234,9 @@ class Test(tk.Frame):
         bantal = 0
         starttid = time.time()
         for bantal in range (10):
-                pygame.mixer.music.load("Frekvensafspiller/Lydfiler/8000Hz_1_L.mp3")
-                pygame.mixer.music.play()
-                bantal +=1
+            pygame.mixer.music.load("Frekvensafspiller/Lydfiler/8000Hz_1_L.mp3")
+            pygame.mixer.music.play()
+            bantal +=1
             for ten_loops in range(1000):
                 if knaptrykket():
                     sluttid = time.time()
@@ -253,4 +246,3 @@ class Test(tk.Frame):
                     pygame.mixer.music.set_volume(lydstyrke)
                     increase_volume()
                     lydstyrke = pygame.mixer.music.get_volume()
-
