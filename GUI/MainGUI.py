850147ed0c426cb10c2486a66c2b13b7797f7056
Guldhammer <Guldhammer@Guldhammers-MacBook-Pro-5.local>
2018-12-23 18:31:50 +0100

    nothing much

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -45,7 +45,7 @@ class StartPage(tk.Frame):# definere en class som inharits fra tk.TK
                          font=TITLE_FONT) #definere et label (text)
         label.place(x=300, y=150) #indsætter det definerede label
 
-        Get_Started = tk.Button(self, text="Kom Igang",width = 20, height = 5,
+        Get_Started = tk.Button(self, text="Kom Igang", width = 20, height = 5,
                             command=lambda: master.switch_frame(
                                 Gender.GenderPick)) #definere en knap og dens funktion
         Get_Started.place(x=295, y=240)
--- GUI/Resultat.py
+++ GUI/Resultat.py
@@ -8,8 +8,8 @@ class Result(tk.Frame):
     def __init__(self,master):
         tk.Frame.__init__(self, master)
         label = tk.Label(self, text="Resultat:", font = TITLE_FONT)
-        label.place(x=400, y=100)
+        label.place(x=300, y=100)
 
         New_Test = tk.Button(self, width = 20, height = 5, text ="Ny test",
         command = lambda: master.switch_frame(MainGUI.StartPage))
-        New_Test.place(x=400, y=320)
+        New_Test.place(x=300, y=270)
